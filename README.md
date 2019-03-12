83ae6a75120cb5928d09216b00866e09c3ebdeeb
Jasper <jiunway@gmail.com>
2019-03-12 11:27:38 +0800

    add crud template sdk

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 # schain-sdk
 
-[Reference]
-https://blog.toright.com/posts/4874/%E5%A6%82%E4%BD%95%E7%99%BC%E4%BD%88%E4%BD%A0%E7%9A%84%E3%80%8C%E5%8C%85%E3%80%8D%E5%88%B0-npm-repository.html
+A Javascript sdk for schain
+
+## SDK usage
+### Compatibility
+The SDK itself is implemented in Javscript
+
+### Install
+1) npm i schain_sdk
+
+###Prepare chaincode id & apikey
+Browse [developer console](http://ec2-13-231-26-144.ap-northeast-1.compute.amazonaws.com/). and create an app in My app then get chaincode id and get api key in profile.
+
+### Use the SDK
+
+####Import schain_sdk in js
+const schain = require('schain_sdk');
+
+####Register user
+let result = await schain.register(USER_NAME, APIKEY);
+
+####Invoke chain code
+let result = await schain.invokeChainCode(APIKEY, CHAINCODE_ID, USER_NAME, FUNCTION, ARGS);
+
+####Query chain code
+let result = await schain.queryChainCode(APIKEY, CHAINCODE_ID, USER_NAME, 'FUNCTION', ARGS);
+
+## License
+Copyright 2019 S-Chain Technologies Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- index.js
+++ index.js
@@ -59,6 +59,23 @@ sdk.register = async (name, apikey) => {
   })
 };
 
+//==========CURD Template SDK==========
+sdk.get = async (apikey, chain_code_id, args) => {
+  console.log("exec get fcn in curd template");
+  return queryChainCode(apikey, chain_code_id, "", "get", args)
+}
+
+sdk.set = async (apikey, chain_code_id, args) => {
+  console.log("exec set fcn in curd template");
+  return invokeChainCode(apikey, chain_code_id, "", "set", args)
+}
+
+sdk.delete = async (apikey, chain_code_id, args) => {
+  console.log("exec del fcn in curd template");
+  return invokeChainCode(apikey, chain_code_id, "", "delete", args)
+}
+
+//====================================
 sdk.invokeChainCode = async (apikey, chain_code_id, user_name, fcn, args) => {
   console.log("exec invoke chain code");
   return execChainCode(CHAIN_CODE_EXEC.INVOKE, chain_code_id, user_name, fcn, args, apikey);
@@ -71,14 +88,6 @@ sdk.queryChainCode = async (apikey, chain_code_id, user_name, fcn, args) => {
 
 execChainCode = async (exec, chain_code_id, user_name, fcn, args, apikey) => {
 
-  if (!fs.existsSync(KEYSTORE + "/" + user_name)) {
-    return {
-      error: "User is not existed"
-    };
-  }
-
-  const user_private_key = fs.readFileSync(KEYSTORE + "/" + user_name, 'utf-8');
-
   var query_url = "";
   if (exec == CHAIN_CODE_EXEC.QUERY) {
     query_url = FW_URL + "/chaincode/" + chain_code_id + "/query"
@@ -86,34 +95,46 @@ execChainCode = async (exec, chain_code_id, user_name, fcn, args, apikey) => {
     query_url = FW_URL + "/chaincode/" + chain_code_id + "/invoke"
   }
 
+  var user_private_key = "";
+  var signature = "";
+  if (user_name) {
+    if (!fs.existsSync(KEYSTORE + "/" + user_name)) {
+      return {
+        error: "User is not existed"
+      };
+    }
+
+    user_private_key = fs.readFileSync(KEYSTORE + "/" + user_name, 'utf-8');
+    user_private_key = user_private_key.trim();
+
+    const msg = JSON.stringify({
+      fcn: fcn,
+      args: JSON.parse(args || "[]")
+    });
+
+    const signer = crypto.createSign('sha256');
+    signer.update(msg);
+    signer.end();
+
+    //const key = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg6ILfw4Zm3fECs9C5x2dMO4yEcOH41EsfEh2zfozMMaWhRANCAAQd9GPXR/0C8i643DOhYTyGXUCS0lnAYpx7Lxz0ykbEeGgGf5sJwZ2W9qlbwhBY0j7pSOsXWZe90YzdIYp2su2z\n-----END PRIVATE KEY-----";
+    try {
+      signature = signer.sign(user_private_key).toString("hex");
+    } catch (err) {
+      console.error(err);
+      return {
+        error: "Private key is not correct"
+      };
+    }
+  }
+
   console.debug(tag, "apikey:", apikey);
   console.debug(tag, 'chain_code_url:', query_url);
   console.debug(tag, 'chain_code_id:', chain_code_id);
   console.debug(tag, 'user_name:', user_name);
-  console.debug(tag, 'user_private_key:', user_private_key.trim());
+  console.debug(tag, 'user_private_key:', user_private_key);
+  console.debug(tag, 'signature:', signature);
   console.debug(tag, 'fcn:', fcn);
   console.debug(tag, 'args:', args);
-
-  const msg = JSON.stringify({
-    fcn: fcn,
-    args: JSON.parse(args || "[]")
-  });
-
-  const signer = crypto.createSign('sha256');
-  signer.update(msg);
-  signer.end();
-
-  //const key = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg6ILfw4Zm3fECs9C5x2dMO4yEcOH41EsfEh2zfozMMaWhRANCAAQd9GPXR/0C8i643DOhYTyGXUCS0lnAYpx7Lxz0ykbEeGgGf5sJwZ2W9qlbwhBY0j7pSOsXWZe90YzdIYp2su2z\n-----END PRIVATE KEY-----";
-  var signature = "";
-  try {
-    signature = signer.sign(user_private_key).toString("hex");
-    console.debug(tag, 'signature:', signature);
-  } catch (err) {
-    console.error(err);
-    return {
-      error: "Private key is not correct"
-    };
-  }
 
   var options = {
     url: query_url,
